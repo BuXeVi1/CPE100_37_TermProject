e29eb3a940ca8ed3d1dc4e8e7e323f939b71fa2d
Kanyaphat10 <kanyaphatying25@gmail.com>
2023-11-24 23:19:07 +0700

    full function

--- Snake-Kanyaphat10/Kanyaphat10/snake-4.c
+++ Snake-Kanyaphat10/Kanyaphat10/snake-4.c
@@ -17,6 +17,8 @@ int speed;
 int option;
 int quit;
 int heartsEaten = 0;
+int fileCheck=0;
+
 
 void Run();
 void delay(int mode);
@@ -39,75 +41,90 @@ void delay(int mode) {
 void runSnakeGame() {
     // Hide cursor
     printf("\e[?25l");
-
+    // oldt = old terminal newt = new terminal
     // Switch to canonical mode, disable echo
-    struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    /*The termios functions describe a general terminal interface that
+       is provided to control asynchronous communications ports.*/
 
+    //  struct termios -> c_lflag -> ICANON, ECHO
+
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt); //STDIN_FILENO value is 0. 
+    //tcgetattr() get the parameters associated with the terminal มาใส่ไว้ &oldt (struct terminal)
+    newt = oldt;// copy oldt to newt
+    newt.c_lflag &= ~(ICANON | ECHO);//close ICANON This allows user input to be sent immediately to the program without waiting for the Enter key to be pressed.
+    //close ECHO ทำให้ข้อมูลที่ผู้ใช้ป้อนไม่ถูกแสดงผลที่หน้าจอ
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);// tcsetattr To customize the behavior of the terminal
+    //TCSANOW The change should take place immediately. ทำการตั้งค่าทันที.
     int x[1000], y[1000];
     quit = 0;
-    while (!quit) {
-        // ... (rest of the code remains the same)
-        // Render table
+    while (!quit) { // quit = 1
+    // Render table
     printf("┌");
-    for (int i = 0; i < COLS; i++)
-      printf("─");
+    for (int i = 0; i < COLS; i++){
+        printf("─");
+    }
     printf("┐\n");
 
     for (int j = 0; j < ROWS; j++) {
       printf("│");
-      for (int i = 0; i < COLS; i++)
+      for (int i = 0; i < COLS; i++){
         printf("·");
+      }
       printf("│\n");
     }
 
     printf("└");
-    for (int i = 0; i < COLS; i++)
-      printf("─");
+    for (int i = 0; i < COLS; i++){
+        printf("─");
+    }
     printf("┘\n");
 
-    // Move cursor back to top
-    printf("\e[%iA", ROWS + 2);
+    // Move cursor back to row+2
+    printf("\e[%iA", ROWS+2);//Set the snake's travel so that the travel area is equal to the display grid.
+    /*%i takes an integer value as an integer value with decimal, hexadecimal, or octal type.
+    To enter a value in hexadecimal format, the value should be provided by preceding “0x” and to enter a value in value in octal format, the value should be provided by preceding “0”.*/
 
     int head = 0, tail = 0;
     x[head] = COLS / 2;
     y[head] = ROWS / 2;
     int gameover = 0;
     int xdir = 1, ydir = 0;
-    int applex = -1, appley;
+    int heartx = -1, hearty;
     
 
     while (!quit && !gameover) {
-      if (applex < 0) {
+      if (heartx < 0) {
         // Create new apple
-        applex = rand() % COLS;
-        appley = rand() % ROWS;
+        heartx = rand() % COLS;
+        hearty = rand() % ROWS;
 
-        for (int i = tail; i != head; i = (i + 1) % 1000)
-          if (x[i] == applex && y[i] == appley)
-            applex = -1;
+        for (int i = tail; i != head; i = (i + 1) % 1000)//circular buffer = the head pointer is incremented and likewise, when the data is being removed (read) the tail pointer is incremented
+          if (x[i] == heartx && y[i] == hearty)
+            heartx = -1;
 
-        if (applex >= 0) {
+        if (heartx >= 0) {
           // Draw apple
-          printf("\e[%iB\e[%iC❤", appley + 1, applex + 1);
-          printf("\e[%iF", appley + 1);
+          printf("\e[%iB\e[%iC❤", hearty + 1, heartx + 1);//\e[%iB\e[%iC is escape sequence =Move the cursor to the specified location.
+          printf("\e[%iF", hearty + 1);
+          //This is to check that a heart (❤) has not been eaten (or the heart position is not set to -1) before drawing an apple on the screen.
         }
       }
 
-      // Clear snake tail
+      // Clear snake tail  Type a dot to replace the moving tail.
       printf("\e[%iB\e[%iC·", y[tail] + 1, x[tail] + 1);
       printf("\e[%iF", y[tail] + 1);
 
-      if (x[head] == applex && y[head] == appley) {
-        applex = -1;
+      if (x[head] == heartx && y[head] == hearty) {
+        heartx = -1;
+        //heart ถูกกิน
         heartsEaten++;// Increase the number of apples eaten by one.
-      } else
+      } else{
         tail = (tail + 1) % 1000;
-
-      int newhead = (head + 1) % 1000;
+      }
+        
+        //The snake's head moves to the next position in the specified direction (xdir, ydir).
+      int newhead = (head + 1) % 1000; // new head position calculate
       x[newhead] = (x[head] + xdir + COLS) % COLS;
       y[newhead] = (y[head] + ydir + ROWS) % ROWS;
       head = newhead;
@@ -115,39 +132,41 @@ void runSnakeGame() {
       for (int i = tail; i != head; i = (i + 1) % 1000)
         if (x[i] == x[head] && y[i] == y[head])
           gameover = 1;
+          //The head of the snake collides with the body, ending the game.
 
       // Draw head
-      printf("\e[%iB\e[%iC▓", y[head] + 1, x[head] + 1);
-      printf("\e[%iF", y[head] + 1);
-      fflush(stdout);
+      printf("\e[%iB\e[%iC▓", y[head] + 1, x[head] + 1);//ค่า head เปลี่ยนเรื่อยๆ ก็จะวาดหัวงูที่ตำแหน่งใหม่เรื่อยๆ
+      printf("\e[%iF", y[head] + 1);//เลื่อน cursor
+      fflush(stdout);//แสดงข้อมูลทันที
 
-      usleep(speed);
+      usleep(speed);//ความเร็วของการเคลื่อนที่ของงูในเกม
 
       // Read keyboard
       struct timeval tv;
-      fd_set fds;
-      tv.tv_sec = 0;
-      tv.tv_usec = 0;
+      fd_set fds;//fds Check the status of STDIN_FILENO fds = Disk file related to receiving data from the keyboard.
+      //ช่วยให้โปรแกรมสามารถตรวจสอบว่ามีการกดปุ่มบนคีย์บอร์ดหรือไม่, โดยไม่ต้องรอเวลานานก่อนที่จะดำเนินการต่อ.
+      tv.tv_sec = 0;// 0 = ไม่มีการรอ วินาที. ในการใช้ฟังก์ชัน select()
+      tv.tv_usec = 0;// 0 = ไม่มีการรอ ไมครอนด์วินาที. ในการใช้ฟังก์ชัน select()
 
-      FD_ZERO(&fds);
-      FD_SET(STDIN_FILENO, &fds);
-      select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
-      if (FD_ISSET(STDIN_FILENO, &fds)) {
+      FD_ZERO(&fds);//ไม่มีไฟล์ดิสก์ที่ถูกตรวจสอบสถานะอยู่. ZERO
+      FD_SET(STDIN_FILENO, &fds); //เพิ่ม STDIN_FILENO = 0
+      select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);// +1 เพื่อให้ select ทำงานถูกต้อง เพื่อเป็นค่าสูงสุด อ่านไฟล์ดิสก์ fds กำหนดเวลา timeout &tv
+      if (FD_ISSET(STDIN_FILENO, &fds)) {//ตรวจสอบได้ว่ามีข้อมูล (input) ที่พร้อมให้อ่านจากคีย์บอร์ด
         int ch = getchar();
-        if (ch == 27 || ch == 'q') {
-          quit = 1;
-        } else if (ch == 'a' && xdir != 1) {
-          xdir = -1;
-          ydir = 0;
-        } else if (ch == 'd' && xdir != -1) {
-          xdir = 1;
-          ydir = 0;
-        } else if (ch == 's' && ydir != -1) {
-          xdir = 0;
-          ydir = 1;
-        } else if (ch == 'w' && ydir != 1) {
-          xdir = 0;
-          ydir = -1;
+        if (ch == 27 ) {//escape
+          quit = 1;//End of program
+        } else if (ch == 'a' && xdir != 1) {//can't move back to right
+          xdir = -1;//press a move left
+          ydir = 0;//does not move on the y axis
+        } else if (ch == 'd' && xdir != -1) {//can't move back to left
+          xdir = 1;//press d move right
+          ydir = 0;//does not move on the y axis
+        } else if (ch == 's' && ydir != -1) {//can't move back to down
+          xdir = 0;//does not move on the x axis
+          ydir = 1;//press s move down
+        } else if (ch == 'w' && ydir != 1) {//can't move back to up
+          xdir = 0;//does not move on the x axis
+          ydir = -1;//press w move up
         }
       }
     }
@@ -155,12 +174,12 @@ void runSnakeGame() {
         if (!quit) {
             // Show game over
             printf("\e[%iB\e[%iC Game Over! ", ROWS / 2, COLS / 2 - 5);
-            printf("\n\n\n\n\n\n\nHearts Eaten: %d\n", heartsEaten);
-            printf("\e[%iF", ROWS / 5);
+            printf("\n\n\n\n\n\n\nHearts Eaten: %d\n", heartsEaten);//show heart that snake eaten
+            printf("\e[%iF", ROWS / 5);//เลื่อนตำแหน่งของ cursor ไปที่บรรทัดที่กำหนดโดยค่า 
             printf("\n");
             // Show cursor
             printf("\e[?25h"); 
-            fflush(stdout);
+            fflush(stdout);//แสดงผลทันที
         }
         printf("\n\nDo you want to continue(y/n)? ");
             char ch;
@@ -174,6 +193,7 @@ void runSnakeGame() {
                     break;
                 case 'n':
                     quit = 1;
+                    fileCheck = option;
                     option = 5;
                     printf("\nThank you for your attention.\n");
                     break;
@@ -182,7 +202,7 @@ void runSnakeGame() {
             }
         } while (ch != 'y' && ch != 'n');
     }
-    // Reset terminal settings
+    // Reset terminal settings  กลับไปการตั้งค่าเริ่มต้น
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     
 }
@@ -232,7 +252,7 @@ void insertionSort(struct Student students[], int count) {
     }
 }
 
-void writeToFile(struct Student students[], int count, const char *fileName) {
+void writeToFile(struct Student students[], int count, const char *fileName) {//ไม่อนุญาตให้ฟังก์ชันนี้เปลี่ยนแปลงชื่อไฟล์ที่ถูกส่งเข้ามา
     FILE *file = fopen(fileName, "w");
     if (file == NULL) {
         printf("Error: Cannot open file %s\n", fileName);
@@ -249,11 +269,28 @@ void writeToFile(struct Student students[], int count, const char *fileName) {
 int play() {
     char fileName[100];
     int check=1;
+    char outputFileName[100];
+    if (fileCheck==1)
+    {
+        snprintf(outputFileName, sizeof(outputFileName), "easy.csv");
+    }
+    else if (fileCheck==2)
+    {
+        snprintf(outputFileName, sizeof(outputFileName), "normal.csv");
+    }
+    else if (fileCheck==3)
+    {
+        snprintf(outputFileName, sizeof(outputFileName), "hard.csv");
+    }
+    else if (fileCheck) 
+    {
+        snprintf(outputFileName, sizeof(outputFileName), "god.csv");//ป้องกัน buffer overflow 
+    }
     
 
-    FILE *file = fopen("rank.csv", "r"), *file2 = fopen("rank.csv", "a+");
+    FILE *file = fopen(outputFileName, "r"), *file2 = fopen(outputFileName, "a+");
     if (file == NULL) {
-        file = fopen("rank.csv", "w");
+        file = fopen(outputFileName, "w");
         if (file != NULL) {
             printf("The file is created.\n");
         } else {
@@ -306,7 +343,7 @@ int play() {
     for(int i = 0; i < numStudents; i++){
         printf("Student %d: %s, Score = %d\n", i+1, students[i].name, students[i].score);
     }
-    char outputFileName[100]={"rank.csv"};
+    
 
     writeToFile(students, numStudents, outputFileName);
     printf("End of program. Goodbye.\n");
