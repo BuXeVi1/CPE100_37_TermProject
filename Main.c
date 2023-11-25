0b45ef8dfb1803cbce84b13cbe0988b7f0bc7740
BuXeVi1Workspace <arkkhanirut.pandej@gmail.com>
2023-11-25 21:43:37 +0700

    update TTT fuunction searchGameHistoryByPlayerName

--- Main.c
+++ Main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "TicTacToe-LuXeVi1/TicTacTor-3.h"
-#include "GuessingNumbers-LuXeVi1/GuessingNumbers-1.h"
+// #include "TicTacToe-LuXeVi1/TicTacTor-3.h"
+// #include "GuessingNumbers-LuXeVi1/GuessingNumbers-1.h"
 
 int main() {
     srand(time(NULL));
@@ -15,7 +15,8 @@ int main() {
         scanf("%d", &choice);
         switch (choice) {
             case 1:
-                playGameTicTacTor();
+                // playGameTicTacTor();
+                system("C:/Users/akani/OneDrive/Documents/VSC/InfinityArcadeByTACTYCAM/TicTacToe-LuXeVi1/TicTacTor-3.exe");
                 break;
             // case 2:
             //     playGuessNumber();
@@ -39,44 +40,3 @@ int main() {
 
     return 0;
 }
-
-// int playGameTicTacTor() {
-//     srand(time(NULL));
-//     printf("Welcome to Tic Tac Toe!\n");
-//     int choice;
-//     do {
-//         printf("\nMenu:\n");
-//         printf("1. Play against a friend\n");
-//         printf("2. Play against the computer\n");
-//         printf("3. View game results\n");
-//         printf("4. Save game history to CSV\n");
-//         printf("5. View game results from CSV\n");
-//         printf("6. Exit\n");
-//         printf("Enter your choice: ");
-//         scanf("%d", &choice);
-//         switch (choice) {
-//             case 1:
-//                 playGame();
-//                 break;
-//             case 2:
-//                 playAgainstComputer();
-//                 break;
-//             case 3:
-//                 viewGameResults();
-//                 break;
-//             case 4:
-//                 saveGameHistoryToCSV();
-//                 break;
-//             case 5:
-//                 readGameHistoryFromCSV();
-//                 break;
-//             case 6:
-//                 printf("End of program. Goodbye!\n");
-//                 break;
-//             default:
-//                 printf("Invalid choice. Please try again.\n");
-//         }
-//     } while (choice != 6);
-
-//     return 0;
-// }
---
+++ TicTacToe-LuXeVi1/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+tory[i].playerName;
+            // printf("%s\t%s\t%d\t%s\n", opponent, gameHistory[i].winner, gameHistory[i].roundsPlayed, gameHistory[i].dateTime);
